6f38c20185f74323100fa49b1f644836788619a4
Aliaksandr <MY_NAME@example.com>
2024-05-24 21:08:08 +0300

    выполнил дз1

---
+++ src/core/lesson1/Main.java
@@ -0,0 +1,10 @@
+package core.lesson1;
+
+public class Main {
+
+    private static final int salary = 5000;
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/core/lesson2/Runner.java
@@ -0,0 +1,11 @@
+package core.lesson2;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Person ivan = new Person();
+        ivan.setName("Иван");
+        System.out.println(ivan.getName());
+    }
+
+}
